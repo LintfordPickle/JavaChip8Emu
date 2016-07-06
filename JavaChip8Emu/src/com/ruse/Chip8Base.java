d6f11daf01c35fa50bb06572b688f640bb54b641
LintfordPickle <jonnohampson@gmail.com>
2016-07-06 19:54:37 +0200

    utilises new openWindow method on JavaBase

--- JavaChip8Emu/src/com/ruse/Chip8Base.java
+++ JavaChip8Emu/src/com/ruse/Chip8Base.java
@@ -3,18 +3,9 @@
  */
 package com.ruse;
 
-import java.awt.BorderLayout;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
 import com.ruse.javabase.Game;
 import com.ruse.javabase.JavaBase;
 
-/**
- * @author Lintford Pickle
- *
- */
 public class Chip8Base {
 
 	// ---------------------------------------
@@ -22,22 +13,9 @@ public class Chip8Base {
 	// ---------------------------------------
 
 	public static void main(String[] args) {
-		JavaBase lBase = new Game();
-
-		JFrame lFrame = new JFrame("Test");
-
-		JPanel lPanel = new JPanel(new BorderLayout());
-		lPanel.add(lBase, BorderLayout.CENTER);
-
-		lFrame.setContentPane(lPanel);
-		lFrame.pack();
-		lFrame.setLocationRelativeTo(null);
-		lFrame.setResizable(true);
-		lFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		lFrame.setVisible(true);
-
-		lBase.start();
-
+		JavaBase lBase = new Game("new window");
+		lBase.openWindow();
+		
 	}
 
 }
