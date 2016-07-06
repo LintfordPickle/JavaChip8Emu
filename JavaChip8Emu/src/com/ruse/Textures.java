4420101532d5bba45fe7cb10680aa66839dd83c5
LintfordPickle <jonnohampson@gmail.com>
2016-07-06 20:29:33 +0200

    setup basic emu window and update/render loops

---
+++ JavaChip8Emu/src/com/ruse/Textures.java
@@ -0,0 +1,10 @@
+package com.ruse;
+
+import com.ruse.javabase.graphics.Art;
+import com.ruse.javabase.graphics.Bitmap;
+
+public class Textures extends Art {
+
+	public static Bitmap chip8 = loadBitmap("/art/chip8.png");
+
+}
